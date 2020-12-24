80b4b2f6c1b725b730537ebfe077a424da9e0265
Diegooliveyra <diegooliveirabryto@gmail.com>
2020-12-24 09:05:20 -0300

    refatorando o codigo

--- public/script/modules/reset-data.js
+++
@@ -1,9 +0,0 @@
-export default function resetaData() {
-  const dias = document.querySelector('[data-contador="dias"]').innerText = '';
-  const horas = document.querySelector('[data-contador="horas"]').innerText = '';
-  const minutos = document.querySelector('[data-contador="minutos"]').innerText = '';
-  const segundos = document.querySelector('[data-contador="segundos"]').innerText = '';
-
-  dias.innerText = '';
-
-}
